9ba4f197937285006f32ad2bdca908f8254e3946
Christian FM <cfernandezmancini@gmail.com>
2023-12-04 23:34:25 -0300

    Frontend - Get API
    Se ramar fronts de las distintas categorias

---
+++ js/listawhisky.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('https://cacgrupo5.pythonanywhere.com/productos')
+        .then(response => response.json())
+        .then(data => {
+            const productos = data.productos;
+            const productosWhisky = productos.filter(producto => producto.categoria.toLowerCase() === 'whisky');
+            let html = '';
+
+            productosWhisky.forEach(producto => {
+                html += `<div class="col-md-4 mb-4">
+                            <div class="card" style="width: 18rem;">
+                                <img src="../assets/whisky2.png" class="card-img-top" alt="whisky">
+                                <div class="card-body">
+                                    <h5 class="card-title">$${producto.precio}</h5>
+                                    <p class="card-text">${producto.nombre} ${producto.tamano_cc}cc</p>
+                                    <p>${producto.subcategoria}</p>
+                                    <a href="#" class="btn btn-primary">Agregar</a>
+                                </div>
+                            </div>
+                         </div>`;
+            });
+
+            document.getElementById('productos-container').innerHTML = html;
+        })
+        .catch(error => console.error('Error:', error));
+});
+
+
